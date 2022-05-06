8c5c40c24598a2be3016a1b12e44eb954b00b017
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-05-06 17:10:37 -0300

    Funcionando

---
+++ multas/pages/index.js
@@ -0,0 +1,77 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import React from 'react';
+
+export default function Home(multas) {
+
+  const multas2 = multas.multas.RETORNO;
+
+  return (
+    <div>
+      <Head>
+        <title>Multas Biblioteca</title>
+        <meta content="Multas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      <div>
+        <h1>Lista de Multas</h1>
+        <table className={styles.tabela} id='tabela'>
+          <thead>
+            <tr className={styles.linha}>
+              <td className={styles.coluna}>TITULO</td>
+              <td className={styles.coluna}>RA</td>
+              <td className={styles.coluna}>NOME</td>
+              <td className={styles.coluna}>DATA EPRESTIMO</td>
+              <td className={styles.coluna}>DATA MULTA</td>
+              <td className={styles.coluna}>VALOR</td>
+              <td className={styles.coluna}>FLAG</td>
+            </tr>
+          </thead>
+          <tbody>
+            {multas2.map(multa => {
+            return <tr className={styles.listaMulta}><td>{multa.num_titulo}</td><td>{multa.ra}</td><td>{multa.nome_pessoa}</td><td>{multa.data_emprestimo}</td><td>{multa.data_multa}</td><td>{multa.valor_multa}</td><td>{multa.flag_transporte}</td><td><button onClick={(event) => alteraMulta(multa, event)}>Baixar</button></td></tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      </main>
+
+      <footer className={styles.footer}>
+        NIU
+      </footer>
+    </div>
+  )
+  }
+
+  const alteraMulta = (a, b) => {
+    
+    var conf = confirm("Tem Certeza que deseja baixar a multa do(a) " + a.nome_pessoa);
+    if(conf == true){
+      baixaMulta(a.num_titulo)
+      alert(a.nome_pessoa + "Baixado com sucesso!")
+      window.location.reload();
+    }else{
+      return
+    }
+  }
+
+  async function baixaMulta(item){
+    await axios.post('http://localhost:4448/AlteraStatus', {
+      'COD': item
+    })
+    console.log("Removeu multa " + item);
+  }
+
+  export async function getServerSideProps(context) {
+    var multas = await axios.get('http://localhost:4448/Lista')
+    
+    multas = multas.data
+  
+    return {
+      props: {multas}, // will be passed to the page component as props
+    }
+  }
