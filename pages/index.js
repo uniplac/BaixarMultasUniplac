480705331202307ddf5f59cffaffd52a030b26a3
uniplac <82620049+uniplac@users.noreply.github.com>
2022-05-19 14:34:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,6 @@ export default function Home(multas) {
       <Head>
         <title>Multas Biblioteca</title>
         <meta content="Multas" />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <link rel="icon" href="/logo.ico" />
       </Head>
       <main className={styles.main} id="pagina">
